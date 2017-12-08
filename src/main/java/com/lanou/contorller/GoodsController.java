49214ff758ef43b11fbaba843e455edb41e309db
xylanjing <31611010+xylanjing@users.noreply.github.com>
2017-12-08 20:38:02 +0800

    1.1.3-xy

--- src/main/java/com/lanou/contorller/GoodsController.java
+++ src/main/java/com/lanou/contorller/GoodsController.java
@@ -1,5 +1,6 @@
 package com.lanou.contorller;
 
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.lanou.entity.Goods;
 import com.lanou.service.GoodsService;
 
 @Controller
@@ -34,5 +36,29 @@ public class GoodsController {
 		Map<String, Object> maps = goodsService.SecondGoods(catId, chooseId, pageId, sortId);
 		return maps;
 	}
+	
+	//点击一级分类展示商品
+	@RequestMapping("/homePage")
+	@ResponseBody
+	public Map<String, Object> homePageGoods(@RequestParam("keyId") int keyId, @RequestParam("chooseId") int chooseId,
+			@RequestParam("pageId") int pageId, @RequestParam("sortId") int sortId) {
+		Map<String, Object> maps = goodsService.homePageGoods(keyId, chooseId, pageId, sortId);
+		return maps;
+	}
 
+	//查找明星推荐商品
+	@RequestMapping("/recommond")
+	@ResponseBody
+	public List<Goods> recommondGoods() {
+		List<Goods> goods = goodsService.recommendGoods();
+		return goods;
+	}
+	
+	// 查找清仓商品
+	@RequestMapping("/sale")
+	@ResponseBody
+	public List<Goods> saleOutGoods() {
+		List<Goods> goods = goodsService.saleOutGoods();
+		return goods;
+	}
 }
--- src/main/java/com/lanou/service/GoodsService.java
+++ src/main/java/com/lanou/service/GoodsService.java
@@ -1,6 +1,9 @@
 package com.lanou.service;
 
+import java.util.List;
 import java.util.Map;
+
+import com.lanou.entity.Goods;
 
 public interface GoodsService {
 
@@ -11,4 +14,12 @@ public interface GoodsService {
 	// 根据二级分类的商品进行展示，分页，排序
 	public Map<String, Object> SecondGoods(int catId, int chooseId, int pageId, int sortId);
 
+	//点击一级分类的商品的展示 
+	public Map<String, Object> homePageGoods(int keyId, int chooseId, int pageId, int sortId);
+	
+	//展示明星推荐商品
+	public List<Goods> recommendGoods(); 
+	
+	//展示清仓商品
+	public List<Goods> saleOutGoods(); 
 }
