6e52d6ce77939ba1b4e0346d93ba84aa9cb59221
Joker <997921135@qq.com>
2017-12-13 20:55:32 +0800

    1.3.1-wz

--- src/main/java/com/lanou/service/impl/OrderServiceImpl.java
+++ src/main/java/com/lanou/service/impl/OrderServiceImpl.java
@@ -1,14 +1,27 @@
 package com.lanou.service.impl;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lanou.entity.Cart;
 import com.lanou.entity.Order;
+import com.lanou.service.CartService;
 import com.lanou.service.OrderService;
 
+@Service("orderService")
 public class OrderServiceImpl implements OrderService {
 
+	@Autowired
+	private CartService cartService;
+	
+	@Autowired
+	private OrderService orderService;
+	
 	@Override
 	public Order showOrder(int userId) {
-		
-		
+		List<Cart> carts = cartService.showCart(userId);
 		
 		
 		return null;
